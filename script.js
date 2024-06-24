590c2f572c99ace1c8004a1fcdec922779e8bb78
lucasxs2466 <130997187+lucasxs2466@users.noreply.github.com>
2024-06-24 16:38:04 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// Add event listener to navigation links
+
 document.querySelectorAll('nav a').forEach(link => {
+
     link.addEventListener('click', event => {
+
         event.preventDefault();
+
         const target = event.target.getAttribute('href');
+
+        
         document.querySelector(target).scrollIntoView({ behavior: 'mooth' });
     });
 });
